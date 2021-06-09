a71cafdc94f1b8c72135c06cc144f7c64b5d92cd
Ajay Marathe <ajay@learny.co>
2021-06-09 19:02:55 +0530

    done functionlity of random quote generator project

--- random-quote-generator/script.js
+++ random-quote-generator/script.js
@@ -1,3 +1,16 @@
-function getRandomQuotes(){
-    
+// hide the loader
+document.getElementById('loader').style.display = 'none';
+
+async function getRandomQuotes() {
+  // show loader 
+  document.getElementById('loader').style.display = 'block';
+
+  const response = await fetch('https://api.quotable.io/random')
+  let data = await response.json();
+  if(data){
+    document.getElementById('loader').style.display = 'none';
+  }
+  console.log(data);
+  document.getElementById('author').innerHTML = data.author;
+  document.getElementById('quote').innerHTML = data.content;
 }
