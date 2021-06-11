aa2f71a244eacee1f79ddd9efa5e2e72ed9a6d94
Ajay Marathe <ajay@learny.co>
2021-06-11 12:53:12 +0530

    implemented right and left button logic, also added few ui chagnes .

--- image-slider-project/slider.js
+++ image-slider-project/slider.js
@@ -5,31 +5,40 @@ window.addEventListener('load', (event) => {
   console.log('page is fully loaded');
   var bgImg = "url('images/" + bgImages[0] + "')"
   document.getElementById('slider').style.backgroundImage = bgImg;
-
+  
 });
+const bgImageLength = bgImages.length - 1 ;
+console.log(bgImageLength);
 function right() {
-  // console.log(bgImages.length);
   count += 1;
-  if(bgImages.length>=count){
-    console.log('hello')
+  console.log('count:', count, 'bgImageLength: ', bgImageLength);
+  console.log(count > bgImageLength);
+
+  if(bgImageLength >= count){
+    console.log('i m in right count.')
     this.changeSlider(count);
-  }else if(count>bgImages.length){
+    document.getElementById('left').disabled = false;
+  }else if(count > bgImageLength){
     document.getElementById('right').disabled = true;
   }
-  console.log(count);
+
 }
 
 function left() {
-  // console.log(bgImages.length);
+
   count -= 1;
-  console.log(count);
-  if(count>=0 && count<=bgImages.length ){
-    console.log('hello')
+  console.log('count:', count, 'bgImageLength: ', bgImageLength);
+
+  if(count >= 0 && count <= bgImageLength ){
+    console.log('i m in left');
     this.changeSlider(count);
+    document.getElementById('right').disabled = false;
   }else{
     console.log('count is less than 0')
     document.getElementById('left').disabled = true;
   }
+
+  console.log('left count: ', count);
 }
 
 function changeSlider(slide){
